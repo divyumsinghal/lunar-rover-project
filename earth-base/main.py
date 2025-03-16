30a7a7bd4b027920b164e888b4b05e5ef59bf9f5
divyumsinghal <divyumsinghal@gmail.com>
2025-03-16 21:50:15 +0000

    Added the code to get the main entry point

---
+++ earth-base/main.py
@@ -0,0 +1,46 @@
+import threading
+import socket
+from src.rover_receive import receive_data_from_rover
+from src.rover_send import send_data_to_rover
+from src.get_cmds import start_gui
+from src.recieve_video import receive_video_from_rover
+from src.config import *
+
+send_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+recv_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+recv_socket.bind((LOCAL_IP, EARTH_RECEIVE_CMD_PORT))
+
+video_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+video_socket.bind((LOCAL_IP, VIDEO_PORT))
+
+stop_event = threading.Event()
+
+
+def main():
+
+    print(f"[EARTH BASE] : Initiating Communication With Rover")
+
+    threading.Thread(
+        target=receive_data_from_rover, args=(recv_socket,), daemon=True
+    ).start()
+
+    threading.Thread(
+        target=send_data_to_rover, args=(send_socket,), daemon=True
+    ).start()
+
+    threading.Thread(
+        target=receive_video_from_rover, args=(send_socket,), daemon=True
+    ).start()
+
+    start_gui(command_queue=command_queue, video_queue=video_queue)
+
+    try:
+        stop_event.wait()
+    except KeyboardInterrupt:
+        print("\n[INFO] Shutting down Earth Base...")
+        stop_event.set()
+
+
+if __name__ == "__main__":
+    main()
