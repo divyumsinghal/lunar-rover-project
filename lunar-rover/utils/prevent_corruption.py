ade5421e3fd8474352f024c05f2e117d0519cf07
divyumsinghal <divyumsinghal@gmail.com>
2025-03-16 20:29:02 +0000

    added hmac for better security

--- lunar-rover/utils/prevent_corruption.py
+++ lunar-rover/utils/prevent_corruption.py
@@ -1,8 +1,19 @@
 import reedsolo
+import hmac
+import hashlib
 
 rs = reedsolo.RSCodec(10)
 
 SECRET_KEY = b"SUPER_SECRET_ROVER_KEY"
+
+
+def generate_hmac(data):
+    return hmac.new(SECRET_KEY, data, hashlib.sha256).digest()
+
+
+def verify_hmac(data, received_hmac):
+    calculated_hmac = generate_hmac(data)
+    return hmac.compare_digest(received_hmac, calculated_hmac)
 
 
 def encode_data(data):
@@ -12,13 +23,11 @@ def encode_data(data):
 def decode_data(data):
     try:
         decoded_result = rs.decode(data)
-
         if isinstance(decoded_result, tuple):
             decoded_bytes = decoded_result[0]
         else:
             decoded_bytes = decoded_result
         return decoded_bytes
-
     except reedsolo.ReedSolomonError:
         print("[ERROR] Reed–Solomon unable to recover data.")
         return None
