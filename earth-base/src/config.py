b34b1bfa8bd539b5644caa53512e31776ca6a03d
divyumsinghal <divyumsinghal@gmail.com>
2025-03-16 21:43:20 +0000

    initialised the common global constants

---
+++ earth-base/src/config.py
@@ -0,0 +1,31 @@
+from dotenv import load_dotenv
+import os
+from queue import Queue
+
+load_dotenv(
+    dotenv_path=os.path.join(os.path.dirname(__file__), ".env"),
+    verbose=True,
+    override=False,
+)
+
+LUNAR_ROVER_1_IP = os.getenv("LUNAR_ROVER_1_IP")
+LOCAL_IP = os.getenv("LOCAL_IP")
+
+LUNAR_ROVER_SEND_DATA_PORT = int(os.getenv("LUNAR_ROVER_SEND_DATA_PORT"))
+EARTH_RECEIVE_CMD_PORT = int(os.getenv("LUNAR_ROVER_RECEIVE_CMD_PORT"))
+VIDEO_PORT = int(os.getenv("VIDEO_PORT"))
+
+command_queue = Queue()
+message_type = "Type"
+message_data = "Data"
+ack = "ACK"
+cmd = "CMD"
+video = "VID"
+sens = "SENS"
+temperature = "Temperature"
+
+
+video_queue = Queue()
+video_1 = "video_1"
+video_2 = "video_2"
+video_3 = "video_3"
